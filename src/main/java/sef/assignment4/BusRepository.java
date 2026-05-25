beb26393a6cf0c2e8cda4f781f70e536b8fc10b3
MasterJ-06 <71957127+MasterJ-06@users.noreply.github.com>
2026-05-25 22:09:05 +1000

    Add skeleton code for the repository classes

--- src/main/java/sef/assignment4/BusRepository.java
+++ src/main/java/sef/assignment4/BusRepository.java
@@ -1,5 +1,29 @@
 package sef.assignment4;
 
-public class BusRepository {
+import java.util.ArrayList;
 
+public class BusRepository {
+    private ArrayList<Bus> buses;
+    private int count;
+
+    public BusRepository() {
+        this.count = 0;
+        this.buses = new ArrayList<>();
+    }
+
+    public void addBus(String busID, int capacity, double fuelLevel, String fuelType) {
+
+    }
+
+    public void updateBus(String busID, int capacity, double fuelLevel, String fuelType) {
+
+    }
+
+    public void retrieveBus(String busID) {
+
+    }
+
+    public int countBuses() {
+        return this.count;
+    }
 }
--- src/main/java/sef/assignment4/DriverRepository.java
+++ src/main/java/sef/assignment4/DriverRepository.java
@@ -1,5 +1,29 @@
 package sef.assignment4;
 
-public class DriverRepository {
+import java.util.ArrayList;
 
+public class DriverRepository {
+    private ArrayList<Driver> drivers;
+    private int count;
+
+    public DriverRepository() {
+        this.count = 0;
+        this.drivers = new ArrayList<>();
+    }
+
+    public void addDriver(String driverID, String name, int experienceYears, String licenseType, String address, String birthdate) {
+
+    }
+
+    public void updateDriver(String driverID, int experienceYears, String licenseType, String address, String birthdate) {
+
+    }
+
+    public void retrieveDriver(String driverID) {
+
+    }
+
+    public int countDrivers() {
+        return this.count;
+    }
 }
